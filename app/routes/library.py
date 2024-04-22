af251a50035eaba0b0ddb4b07ec0dca2f56e0467
timoniq <arseny.kriuchkov@gmail.com>
2024-04-22 14:12:27 +0300

    add poems and fixes

--- app/routes/library.py
+++ app/routes/library.py
@@ -1,5 +1,5 @@
 from fastapi import APIRouter, Request
-from fastapi.responses import HTMLResponse, RedirectResponse
+from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
 import os
 import random
